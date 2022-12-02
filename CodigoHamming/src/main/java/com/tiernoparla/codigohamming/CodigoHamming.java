470afe270a3bad5fd37c7d701f2229bdbdf12cfc
AlejandroJ1 <alejandro.jimenezmartin@tiernoparla.es>
2022-12-02 23:32:53 +0100

    Esta es la parte a del ejercicio 2 de Hamming

--- CodigoHamming/src/main/java/com/tiernoparla/codigohamming/CodigoHamming.java
+++ CodigoHamming/src/main/java/com/tiernoparla/codigohamming/CodigoHamming.java
@@ -2,22 +2,48 @@ package com.tiernoparla.codigohamming;
 
 import java.util.Random;
 import java.util.Scanner;
+import java.lang.Math;
 
 public class CodigoHamming {
 
     public static void main(String[] args) {
-        //Primera parte del código hamming con un scanner, un random y un array.
+        int bitsParidad;
+
+        // Primera parte del código hamming con un scanner, un random y un array.
         Scanner scanea = new Scanner(System.in);
         System.out.println("Introduce un número: ");
         int tamaño = scanea.nextInt();
 
         Random num = new Random();
-        int[] mensage = new int[tamaño];
+        int[] msg = new int[tamaño];
         for (int i = 0; i < tamaño; i++) {
-            mensage[i] = num.nextInt(2);
-            System.out.print(mensage[i]);
+            msg[i] = num.nextInt(2);
+            System.out.print(msg[i]);
         }
+        System.out.println();
+        ///////////////
+        //// punto 2////
+        ///////////////
+        bitsParidad = calcularBitsRed(tamaño);
+        System.out.print("bits paridad " + bitsParidad);
 
+        int longMsgHami = tamaño + bitsParidad + mensage.length + 1;
+        int[] msgHamming = new int[longMsgHami];
+
+        System.out.println();
+
+    }// main
+
+    static int calcularBitsRed(int tamaño) {
+
+        int i = 0;
+        int j = 0;
+
+        while ((j) < tamaño) {
+            i++;
+            j = (int) Math.pow(2, i) - (i + 1);
+        }
+        return i;
     }
 
-}
+}// class
