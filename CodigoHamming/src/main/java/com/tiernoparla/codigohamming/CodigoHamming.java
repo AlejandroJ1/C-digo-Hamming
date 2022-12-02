8a4c6c222329c580711e585d4dee0ea34a3c8c74
AlejandroJ1 <alejandro.jimenezmartin@tiernoparla.es>
2022-12-02 23:39:46 +0100

    Esta es la parte b del ejercicio 2 de Hamming

--- CodigoHamming/src/main/java/com/tiernoparla/codigohamming/CodigoHamming.java
+++ CodigoHamming/src/main/java/com/tiernoparla/codigohamming/CodigoHamming.java
@@ -15,10 +15,10 @@ public class CodigoHamming {
         int tamaño = scanea.nextInt();
 
         Random num = new Random();
-        int[] msg = new int[tamaño];
+        int[] mensage = new int[tamaño];
         for (int i = 0; i < tamaño; i++) {
-            msg[i] = num.nextInt(2);
-            System.out.print(msg[i]);
+            mensage[i] = num.nextInt(2);
+            System.out.print(mensage[i]);
         }
         System.out.println();
         ///////////////
@@ -32,6 +32,13 @@ public class CodigoHamming {
 
         System.out.println();
 
+        msgHamming = codigoHamming(longMsgHami, mensage);
+        for (int i = 0; i < msgHamming.length; i++) {
+            if (i == 0 || i == 1 || i == 2 || i == 4 || i == 8 || i == 16)
+                System.out.print("->");
+            System.out.print(msgHamming[i]);
+        }
+
     }// main
 
     static int calcularBitsRed(int tamaño) {
@@ -44,6 +51,100 @@ public class CodigoHamming {
             j = (int) Math.pow(2, i) - (i + 1);
         }
         return i;
+    }
+
+    static int[] codigoHamming(int tamañoTotal, int[] codOriginal) {
+        int msgHamming[] = new int[tamañoTotal];
+        int i = 0;
+        int j = 0;
+        for (i = 0, j = 0; i < msgHamming.length; i++) {
+            if ((i == Math.pow(2, 0) || i == Math.pow(2, 1) || i == Math.pow(2, 2) || i == Math.pow(2, 3) ||
+                    i == Math.pow(2, 4) || i == Math.pow(2, 5) || i == Math.pow(2, 6) || i == Math.pow(2, 7))
+                    || j > codOriginal.length - 1) {
+                continue;
+            } else {
+                msgHamming[i] = codOriginal[j];
+                j++;
+                if (j > codOriginal.length - 1) {
+                    i++;
+                    break;
+                }
+            }
+        }
+        int aux[] = new int[i + 1];
+        while (i + 1 > 0) {
+            aux[i] = msgHamming[i];
+            i--;
+        }
+        activarBitsParidad(aux);
+        return aux;
+    }
+
+    static int[] activarBitsParidad(int[] msg) {
+
+        int contador = 0;
+        //////////// bit paridad 1 ///////////////////////////////////////////////
+        for (int i = 0; i < msg.length; i++) {
+            if (msg[3] == 1 || msg[5] == 1 || msg[7] == 1 || msg[9] == 1 || msg[11] == 1 || msg[13] == 1 || msg[15] == 1
+                    || msg[17] == 1 || msg[19] == 1 || msg[21] == 1) {
+                contador++;
+            }
+            if (contador % 2 == 0)
+                msg[1] = 0;
+            else
+                msg[1] = 1;
+        }
+        ///////// bit paridad 2//////////////////////////////////////////////////////
+        contador = 0;
+        for (int i = 0; i < msg.length; i++) {
+            if (msg[3] == 1 || msg[6] == 1 || msg[7] == 1 || msg[10] == 1 || msg[11] == 1 || msg[14] == 1
+                    || msg[15] == 1 || msg[18] == 1 || msg[19] == 1) {
+                contador++;
+            }
+            if (contador % 2 == 0)
+                msg[2] = 0;
+            else
+                msg[2] = 1;
+        }
+        /////// bit paridad 4 ////////////////////////////////////////////////////
+
+        contador = 0;
+        for (int i = 0; i < msg.length; i++) {
+            if (msg[5] == 1 || msg[6] == 1 || msg[7] == 1 || msg[12] == 1 || msg[13] == 1 || msg[14] == 1
+                    || msg[15] == 1 || msg[20] == 1 || msg[21] == 1) {
+                contador++;
+            }
+            if (contador % 2 == 0)
+                msg[4] = 0;
+            else
+                msg[4] = 1;
+        }
+        /////// bit paridad 8 ////////////////////////////////////////////////////
+
+        contador = 0;
+        for (int i = 0; i < msg.length; i++) {
+            if (msg[9] == 1 || msg[10] == 1 || msg[11] == 1 || msg[12] == 1 || msg[13] == 1 || msg[14] == 1
+                    || msg[15] == 1) {
+                contador++;
+            }
+            if (contador % 2 == 0)
+                msg[8] = 0;
+            else
+                msg[8] = 1;
+        }
+        /////// bit paridad 16 ////////////////////////////////////////////////////
+        contador = 0;
+        for (int i = 0; i < msg.length; i++) {
+            if (msg[17] == 1 || msg[18] == 1 || msg[19] == 1 || msg[20] == 1 || msg[21] == 1) {
+                contador++;
+            }
+            if (contador % 2 == 0)
+                msg[16] = 0;
+            else
+                msg[16] = 1;
+        }
+
+        return msg;
     }
 
 }// class
